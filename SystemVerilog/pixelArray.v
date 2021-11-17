d4cd4ae92f87ab11feee039c7b29a94de1700321
studyjen <83062694+studyjen@users.noreply.github.com>
2021-11-17 16:59:14 +0100

    Add files via upload

---
+++ SystemVerilog/pixelArray.v
@@ -0,0 +1,24 @@
+
+module pixelArray
+	(
+	input logic      VBN1,
+	input logic      RAMP,
+	input logic      RESET,
+	input logic      ERASE,
+	input logic      EXPOSE,
+	input logic      READ12,
+	input logic      READ34,
+		
+	inout [7:0] DATA1,
+	inout [7:0] DATA2,
+	inout [7:0] DATA3,
+	inout [7:0] DATA4
+	);
+	
+	
+	PIXEL_SENSOR p1(VBN1, RAMP, RESET, ERASE, EXPOSE, READ12, DATA1);
+	PIXEL_SENSOR p2(VBN1, RAMP, RESET, ERASE, EXPOSE, READ12, DATA2);
+	PIXEL_SENSOR p3(VBN1, RAMP, RESET, ERASE, EXPOSE, READ34, DATA3);
+	PIXEL_SENSOR p4(VBN1, RAMP, RESET, ERASE, EXPOSE, READ34, DATA4);
+		
+endmodule	
